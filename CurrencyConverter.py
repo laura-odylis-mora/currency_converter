8e242a6bc3b40d25b8c44a5e5f9b1bf745389ad4
Laura <lauraodylis@icloud.com>
2022-04-06 21:43:36 -0400

    Adjusted some dimensions for Window and Convrect

--- CurrencyConverter.py
+++ CurrencyConverter.py
@@ -1,22 +1,23 @@
-#This converter will convert between one world currency to another world currency
-#choosing. The exchange rates are variable but they will be fixed from a certain
-#source and date as noted on the converter; the converter provides an estimate.
+# This converter will convert between one world currency to another world currency
+# choosing. The exchange rates are variable but they will be fixed from a certain
+# source and date as noted on the converter; the converter provides an estimate.
 
 # to import everyting defined in the module
 from graphics import *
 from Button import Button
 from EasyRectangle import EasyRectangle
 
+
 class Converter:
     def __init__(self):
-        self.convrect = EasyRectangle(0, 0, 100, 100)
+        self.convrect = EasyRectangle(0, 0, 176, 176)
         self.convrect.setFill('LightBlue')
 
         self.input_text1 = Entry(Point(-24, 26), 14)
 
-        self.quit_button = Button(44, 44, 8, 8, 'Red', 'X')
+        self.quit_button = Button(82, 82, 8, 8, 'Red', 'X')
         self.clear_button = Button(-23, 0, 30, 10, 'Light Green', 'Clear')
-       
+
         #self.usd1_button = Button()
 
         self.result = EasyRectangle(0, -30, 60, 10)
@@ -35,14 +36,13 @@ class Converter:
     def run(self, win):
         while True:
             p = win.getMouse()  # Waits for mouse click
-            
+
             x1 = self.get_entry_value(self.input_text1)
-            
 
 
 def main():
     # Creates a window and puts the converter object in it
-    win = GraphWin('Currency Converter', 500, 500)
+    win = GraphWin('Currency Converter', 700, 700)
     win.setBackground('cornsilk')
     win.setCoords(-100, -100, 100, 100)
 
@@ -51,5 +51,6 @@ def main():
     conv.run(win)
 
     win.close()
+
 
 main()
