c3a2fba8aabeff28d5c865cdcc1668da91ea4eb0
Laura <lauraodylis@icloud.com>
2022-04-11 16:08:18 -0400

    Worked on match case functions

--- CurrencyConverter.py
+++ CurrencyConverter.py
@@ -2,15 +2,21 @@
 # choosing. The exchange rates are variable but they will be fixed from a certain
 # source and date as noted on the converter; the converter provides an estimate.
 
-# to import everyting defined in the module
+# Library flag emojis
 from graphics import *
 import flag
+
+# to import everyting defined in the module
 from Button import Button
 from EasyRectangle import EasyRectangle
+
+# Exachange Rates and Converter Function API
+from forex_python.converter import CurrencyRates
 
 
 class Converter:
     def __init__(self):
+        # Create Buttons and set graphics window
         self.convrect = EasyRectangle(0, 0, 176, 176)
         self.convrect.setFill('LightBlue')
 
@@ -20,8 +26,6 @@ class Converter:
         self.clear_button = Button(0, 30, 18, 7, 'Ivory', 'Clear')
 
         self.convert_button = Button(0, 50, 24, 7, 'YellowGreen', 'Convert')
-
-        #self.usd1_button = Button()
 
         self.result = EasyRectangle(50, 50, 28, 7)
         self.result.setOutline('GhostWhite')
@@ -79,6 +83,7 @@ class Converter:
         self.usd_2_button = Button(50, -71, 60, 7,
                                    'DarkSalmon', flag.flagize("United States USD :US:"))
 
+    # Draw Buttons and Graphics Window
     def draw(self, win):
         self.convrect.draw(win)
         self.input_text.draw(win)
@@ -111,14 +116,30 @@ class Converter:
         self.usd_2_button.draw(win)
         self.disclaimer.draw(win)
 
+    # Return input as Float otherwise statement about inputting numbers only
+    def get_entry_value(self, entry):
+        try:
+            x = float(entry.getText())
+        except:
+            x = None
+            self.result_text.setText("Input must be a number")
+        return x
+
+    # Display Result
+    def display_result(self, result):
+        self.result_text.setText(str(result))
+        # self.result_text.setText(float(result))
+
     # Clears the display of all text
     def clear(self):
         self.input_text.setText('')
         self.result_text.setText('')
 
+    # Highlight chosen button when button clicked
     def highlightButton(self, country):
         self.country.setFill('DarkSalmon')
 
+    # Unhighlight buttons when "clear" function runs
     def unhighlightButton(self, country):
         self.country.setFill('Gold')
 
@@ -127,9 +148,41 @@ class Converter:
 
     def run(self, win):
         while True:
-            p = win.getMouse()  # Waits for mouse click
+            input_amount = self.get_entry_value(self.input_text)
+            country1 = ""
+            country2 = ""
+            p1 = win.getMouse()  # Waits for mouse click
 
-            x1 = self.get_entry_value(self.input_text1)
+            match p1:
+                case self.clear_button.clicked(p1):
+                    self.clear()
+                case self.quit_button.clicked(p1):
+                    break
+                case self.usd_1_button.clicked(p1):
+                    country1 = 'USD'
+                case _:  # write errror message saying pick what country's currency you starting with
+                    print('some error message')
+
+            p2 = win.getMouse()
+
+            match p2:
+                case self.clear_button.clicked(p2):
+                    self.clear()
+                case self.quit_button.clicked(p2):
+                    break
+                case self.usd_2_button.clicked(p2):
+                    country2 = 'USD'
+                case _:
+                    <action_wildcard >
+
+            p3 = win.getMouse()
+
+            match p3:
+                case self.clear_button.clicked(p3):
+                    self.clear()
+                case self.quit_button.clicked(p3):
+                    break
+                case self.convert_button.clicked(p3):
 
 
 def main():
@@ -140,6 +193,8 @@ def main():
 
     conv = Converter()
     conv.draw(win)
+
+    rate_of_currency = CurrencyRates()
 
     conv.run(win)
 
